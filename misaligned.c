9a8aa45d7c55625aaee0e38d1cc36d190b4633b4
NabilCob <69570628+NabilCob@users.noreply.github.com>
2022-12-06 20:20:33 +0530

    Update misaligned.c
    -Corrected code

--- misaligned.c
+++ misaligned.c
@@ -3,9 +3,8 @@
 #include <assert.h>
 #include "misaligned.h"
 
-
 //Function to print color pattern
-int printColorMap(char * (*GetExpectedColorPair)(int, int)) {
+int printColorMap(void (*GetColorPattern)(char*, int, int)) {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
@@ -13,18 +12,19 @@ int printColorMap(char * (*GetExpectedColorPair)(int, int)) {
     memset(colorPairNames, 0, MAX_COLORPAIR_NAME_CHARS);
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            sprintf(colorPairNames, "%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-            //Assertion for pairnumber and color pattern
-            assert(memcmp(colorPairNames, GetExpectedColorPair(i, j), MAX_COLORPAIR_NAME_CHARS)
-                            == 0);
-
+            sprintf(colorPairNames, "%d | %s | %s\n", i * 5 + j + 1, majorColor[i], minorColor[j]);
+            //Display color patterns
+            printf("\n%s",colorPairNames);
+            if(GetColorPattern != 0){
+                (*GetColorPattern)(colorPairNames, i, j);
+            }
         }
     }
     return i * j;
 }
 
 //Get Expected color pattern
-char* GetExpectedColorPair(int majorIndex, int minorIndex){
+void GetColorPattern(char* colorPattern, int majorIndex, int minorIndex){
 
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
@@ -33,21 +33,18 @@ char* GetExpectedColorPair(int majorIndex, int minorIndex){
     memset(ExpectedColorPairNames, 0, MAX_COLORPAIR_NAME_CHARS);
     sprintf(ExpectedColorPairNames, "%d | %s | %s\n", majorIndex * numberOfMinorColors + minorIndex + 1
                                            , majorColor[majorIndex], minorColor[minorIndex]);
-    return ExpectedColorPairNames;
+    //Assertion for color pattern
+    assert(memcmp(colorPattern, ExpectedColorPairNames, MAX_COLORPAIR_NAME_CHARS) == 0);
 }
 
 
 //Main function
 int main(void){
-    
-    //Assertion for total color patterns
-    assert(printColorMap(GetExpectedColorPair) == 25);
-    
-    printf("\n misaligned file - Bye");
+
+    assert(printColorMap(GetColorPattern) == 25);
+    printf("\nmisaligned file - Bye");
     return 0;
 }
-
-
 
 
 
