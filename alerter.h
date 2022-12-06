76519038a6a6722b8a1e5a01c1f608552fb5c3f5
NabilCob <69570628+NabilCob@users.noreply.github.com>
2022-12-06 20:22:36 +0530

    Update alerter.h
    -Updated function declaration

--- alerter.h
+++ alerter.h
@@ -1,11 +1,8 @@
 #ifndef ALERTER_H_
 #define ALERTER_H_
 
-//Structure, enum and macro definitions
-typedef int (*networkAlerter_Funcptr)(float);
-
 //Function declaration
-void alertInCelcius(float farenheit);
+void alertInCelcius(float farenheit, int (*networkAlerter)(float));
 int networkAlertStub(float celcius);
 
 #endif
