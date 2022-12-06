e1f2c6492d47b9a9c53757c314423e7c4813980d
NabilCob <69570628+NabilCob@users.noreply.github.com>
2022-12-06 20:17:40 +0530

    Update tshirts.c
    - Corrected code

--- tshirts.c
+++ tshirts.c
@@ -6,16 +6,17 @@
 //Function returns the size
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
-        sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if(cms > 42) {
-        sizeName = 'L';
+    if(cms > 0){
+        if(cms < 38){
+            sizeName = 'S';
+        } else if(cms >= 38 && cms < 42){
+            sizeName = 'M';
+        } else if(cms >= 42) {
+            sizeName = 'L';
+        }
     }
     return sizeName;
 }
-
 //Main Function
 int main(void)
 {
@@ -27,6 +28,6 @@ int main(void)
     assert(size(38) == 'M');
     assert(size(42) == 'L');
 
-    printf("n tshirts file - Bye");
+    printf("\ntshirts file - Bye");
     return 0;
 }
